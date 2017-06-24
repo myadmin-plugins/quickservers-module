7c26b7f278783af83c1327e35309d5380ed24677
Joe Huss <detain@interserver.net>
2017-06-24 07:58:40 -0400

    improvmenets to module handling

--- src/Plugin.php
+++ src/Plugin.php
@@ -18,8 +18,8 @@ class Plugin {
 
 	public static function getHooks() {
 		return [
-			'quickservers.load_processing' => [__CLASS__, 'loadProcessing'],
-			'quickservers.settings' => [__CLASS__, 'getSettings'],
+			self::$module.'.load_processing' => [__CLASS__, 'loadProcessing'],
+			self::$module.'.settings' => [__CLASS__, 'getSettings'],
 		];
 	}
 
@@ -29,6 +29,6 @@ class Plugin {
 
 	public static function getSettings(GenericEvent $event) {
 		$settings = $event->getSubject();
-		$settings->add_dropdown_setting('quickservers', 'General', 'outofstock_quickservers', 'Out Of Stock Quickservers', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_QUICKSERVERS'), array('0', '1'), array('No', 'Yes',));
+		$settings->add_dropdown_setting(self::$module, 'General', 'outofstock_quickservers', 'Out Of Stock Quickservers', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_QUICKSERVERS'), array('0', '1'), array('No', 'Yes',));
 	}
 }
