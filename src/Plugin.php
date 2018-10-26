e56269267ea733efcfaa89482473d500a7d19789
Joe Huss <detain@interserver.net>
2018-10-26 10:42:12 -0400

    fixs

--- src/Plugin.php
+++ src/Plugin.php
@@ -101,7 +101,7 @@ class Plugin
 				$smarty = new \TFSmarty;
 				$smarty->assign('qs_name', $serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name']);
 				$email = $smarty->fetch('email/admin/qs_reactivated.tpl');
-				$subject = $db->Record[$settings['TITLE_FIELD']].' '.$serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name'].' '.$settings['TBLNAME'].' Re-Activated';
+				$subject = $serviceInfo[$settings['TITLE_FIELD']].' '.$serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name'].' '.$settings['TBLNAME'].' Re-Activated';
 				$headers = '';
 				$headers .= 'MIME-Version: 1.0'.PHP_EOL;
 				$headers .= 'Content-type: text/html; charset=UTF-8'.PHP_EOL;
