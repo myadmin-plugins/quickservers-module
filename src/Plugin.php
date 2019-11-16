fa2100e5fb9ffacfa5e02bf4265dc12540a8065c
Joe Huss <detain@interserver.net>
2019-11-16 10:50:01 -0500

    a bunch of updates switching out old admin_mail code with the new Mail class adminMail call and optmizing things here and there as i go

--- src/Plugin.php
+++ src/Plugin.php
@@ -102,11 +102,7 @@ class Plugin
 				$smarty->assign('qs_name', $serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name']);
 				$email = $smarty->fetch('email/admin/qs_reactivated.tpl');
 				$subject = $serviceInfo[$settings['TITLE_FIELD']].' '.$serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name'].' '.$settings['TBLNAME'].' Reactivated';
-				$headers = '';
-				$headers .= 'MIME-Version: 1.0'.PHP_EOL;
-				$headers .= 'Content-type: text/html; charset=UTF-8'.PHP_EOL;
-				$headers .= 'From: '.TITLE.' <'.EMAIL_FROM.'>'.PHP_EOL;
-				admin_mail($subject, $email, $headers, false, 'admin/qs_reactivated.tpl');
+				(new MyAdmin\Mail())->adminMail($subject, $email, false, 'admin/qs_reactivated.tpl');
 			})->setDisable(function ($service) {
 			})->setTerminate(function ($service) {
 				$serviceInfo = $service->getServiceInfo();
